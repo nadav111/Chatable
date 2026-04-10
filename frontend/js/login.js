cc78063ffa6ea80af68da7d30f004f6042e0e463
נדב ב <nadavburko@gmail.com>
2026-04-10 11:44:07 +0300

    fix(frontend/dockerfile): Fix front dockerfile - index.html not found issue

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -24,7 +24,7 @@ async function handleLoginSubmit(e) {
 
     if (response.token) {
       localStorage.setItem('userToken', response.token);
-      window.location.href = 'home.html';
+      window.location.href = 'index.html';
     } else {
       // alert only the error message from server
       alert(response.error || 'Login failed.');
